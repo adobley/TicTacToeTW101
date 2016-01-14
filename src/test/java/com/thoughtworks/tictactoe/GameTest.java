846d27695435a4824bf978150f0ae04961f3c0c5
Aidan Obley <aobley@thoughtworks.com>
2016-01-14 12:59:27 -0600

    New red test prompt for input

--- src/test/java/com/thoughtworks/tictactoe/GameTest.java
+++ src/test/java/com/thoughtworks/tictactoe/GameTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.tictactoe;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.PrintStream;
@@ -10,17 +11,30 @@ import static org.mockito.Mockito.verify;
 
 public class GameTest {
 
+    private PrintStream printStream;
+    private Game game;
+
+    @Before
+    public void setup() {
+        printStream = mock(PrintStream.class);
+        game = new Game(printStream);
+    }
+
     @Test
     public void shouldDisplayBoardWhenGameStarts() {
-        PrintStream printStream = mock(PrintStream.class);
-        Game game = new Game(printStream);
-        String correctBoard = "  |   |\n" +
+        String correctBoard =  "  |   |\n" +
                 "---------\n" +
                 "  |   |\n" +
                 "---------\n" +
                 "  |   |";
         game.start();
         verify(printStream).println(correctBoard);
+    }
+
+    @Test
+    public void shouldPromptPlayerWhenGameStarts() {
+        game.start();
+        verify(printStream).println("Enter a number between 1 and 9:");
     }
 
 }

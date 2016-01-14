fbe2c8f17fa0314c85b1f6ee43bb9afccb048a82
Aidan Obley <aobley@thoughtworks.com>
2016-01-14 12:55:05 -0600

    Board prints in production code, but is needs visible changes

--- src/main/java/com/thoughtworks/tictactoe/Main.java
+++ src/main/java/com/thoughtworks/tictactoe/Main.java
@@ -3,6 +3,7 @@ package com.thoughtworks.tictactoe;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Tic Tac Toe");
+        Game game = new Game(System.out);
+        game.start();
     }
 }

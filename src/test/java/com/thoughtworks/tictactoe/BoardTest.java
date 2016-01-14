623633633aa33e2aeb82b8b398f5af4f49ee6762
Aidan Obley <aobley@thoughtworks.com>
2016-01-14 16:29:05 -0600

    Refactored behavior for displaying board Added loop checking input

--- src/test/java/com/thoughtworks/tictactoe/BoardTest.java
+++ src/test/java/com/thoughtworks/tictactoe/BoardTest.java
@@ -4,8 +4,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -13,10 +15,14 @@ public class BoardTest {
 
     private Board board;
     private PrintStream printStream;
+    private List<String> moves;
 
     @Before
     public void setup() {
         printStream = mock(PrintStream.class);
+        Integer boardSize = 9;
+        moves = new ArrayList<String>(Collections.nCopies(boardSize, " "));
+        Collections.fill(moves, " ");
         board = new Board(printStream);
     }
 
@@ -27,7 +33,24 @@ public class BoardTest {
                 "   |   |   \n" +
                 "-----------\n" +
                 "   |   |   \n";
-        board.drawBoard();
+        board.drawBoard(moves);
+        verify(printStream).print(correctBoard);
+    }
+
+    @Test
+    public void shouldPrintBoardWithSymbolsWhenDrawing() {
+        String correctBoard = " X |   |   \n" +
+                "-----------\n" +
+                "   |   | O \n" +
+                "-----------\n" +
+                "   | X |   \n";
+        moves.set(0, "X");
+        moves.set(5, "O");
+        moves.set(7, "X");
+
+        board.drawBoard(moves);
+
+
         verify(printStream).print(correctBoard);
     }
 }

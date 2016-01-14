e301639dd5b854b643c817f1934ef65d3ba76fe4
Aidan Obley <aobley@thoughtworks.com>
2016-01-14 13:11:37 -0600

    All tests green Main runs and takes input from user

--- src/main/java/com/thoughtworks/tictactoe/Game.java
+++ src/main/java/com/thoughtworks/tictactoe/Game.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.tictactoe;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 
 public class Game {
@@ -11,14 +12,20 @@ public class Game {
             "---------\n" +
             "  |   |";
     private PrintStream printStream;
+    private BufferedReader bufferedReader;
 
     public Game(PrintStream printStream, BufferedReader bufferedReader) {
-
         this.printStream = printStream;
+        this.bufferedReader = bufferedReader;
     }
 
     public void start() {
         printStream.println(board);
         printStream.println("Enter a number between 1 and 9:");
+        try {
+            String input = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
